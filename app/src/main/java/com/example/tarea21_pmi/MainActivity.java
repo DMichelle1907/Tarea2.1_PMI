b47e2afe70ee67d74b187e3784935cdf46531a73
Daniela Reyes <reyesdaniela498@gmail.com>
2023-11-10 17:58:48 -0600

    Grabar video

--- app/src/main/java/com/example/tarea21_pmi/MainActivity.java
+++ app/src/main/java/com/example/tarea21_pmi/MainActivity.java
@@ -1,14 +1,153 @@
 package com.example.tarea21_pmi;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.icu.text.SimpleDateFormat;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    static final int Peticion_AccesoCamara = 101;
+    static final int Peticion_GrabarVideo = 102;
+    Button btnGrabar, btnGuardar;
+    VideoView videoView;
+    String currentVideoPath;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        videoView = findViewById(R.id.Video);
+        btnGrabar = findViewById(R.id.btnGrabar);
+        btnGuardar = findViewById(R.id.btnGuardarVideo);
+        btnGrabar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Permisos();
+            }
+        });
+
+        btnGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                GrabarVideo();
+            }
+        });
+
+
+    }
+
+    private void Permisos() {
+        if(ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+
+        {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                    Peticion_AccesoCamara);
+
+        }
+        else
+        {
+            dispatchTakePictureIntent();
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == Peticion_AccesoCamara){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                //Grabar video
+                dispatchTakePictureIntent();
+            }else{
+                Toast.makeText(getApplicationContext(), "Permiso denegado", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void GrabarVideo() {
+
+        File directorioDestino = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_MOVIES), "VideosGuardados");
+
+        if (!directorioDestino.exists()) {
+            directorioDestino.mkdirs();
+
+        }
+
+        Toast.makeText(getApplicationContext(), "Video guardado", Toast.LENGTH_LONG).show();
+
+
+    }
+
+
+    private File createVideoFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String videoFileName = "MP4_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_MOVIES);
+        File video = File.createTempFile(
+                videoFileName,  /* prefix */
+                ".mp4",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        currentVideoPath = video.getAbsolutePath();
+        return video;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+
+                startActivityForResult(takePictureIntent, Peticion_GrabarVideo);
+            }
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, Peticion_AccesoCamara);
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == Peticion_GrabarVideo && resultCode == RESULT_OK){
+
+            try {
+                File foto = new File(currentVideoPath);
+                videoView.setVideoURI(Uri.fromFile(foto));
+
+            }catch (Exception ex) {
+                ex.toString();
+            }
+
+        }
+
     }
 }
